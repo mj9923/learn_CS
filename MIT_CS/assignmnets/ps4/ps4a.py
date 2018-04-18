b4694ce63a145879d872fa3287c3593cb2fda818
mj9923 <mj9923@snu.ac.kr>
2018-04-19 01:25:18 +0900

    permutation problem

--- MIT_CS/assignmnets/ps4/ps4a.py
+++ MIT_CS/assignmnets/ps4/ps4a.py
@@ -23,7 +23,27 @@ def get_permutations(sequence):
     a different order than what is listed here.
     '''
     permutation_list = []
-    for letter in sequence:
+    if len(sequence) == 1:
+        permutation_list.append(sequence)
+    elif len(sequence) == 2:
+        permutation_list.extend(sequence, sequence[1]+sequence[0])
+    else:
+        #create letter_list
+        letter_list = []
+        for i in range(len(sequence)):
+            letter_list.append(sequence[i])
+
+        for i in range(len(sequence)):
+            llc = letter_list
+            num = len(sequence)
+            word_list=[sequence[i]]
+            word_list_2
+            while num != 0:
+                for j in range(num-1):
+                    for k in range(len(word_list)):
+                        pos = word_list[k]+
+                        word_list_2.append()
+                num -= 1
 
 
 if __name__ == '__main__':
